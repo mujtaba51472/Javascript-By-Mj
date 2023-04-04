c4b01dae8623b3ece0564fe7c8425ed076504e1b
mujtaba514 <mujtaba51472@gmail.com>
2023-04-04 23:51:22 +0500

    add varibales section first part

---
+++ Varibles In Js/index.js
@@ -0,0 +1,11 @@
+// __________________deifined vs not defined vs undefined ,,,,,, start__________________________
+// not defined 
+// console.log(z); // Output: Uncaught ReferenceError: z is not defined
+
+// undefined 
+// var y;
+// console.log(y); // Output: undefined
+
+// __________________deifined vs not defined vs undefined ,,,,,, end _____________________
+
+
